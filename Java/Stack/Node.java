72aff6c646e1d5426c886abd03a8a03bed846dbc
Jay Lee <thecodingdelight@gmail.com>
2017-04-06 16:45:22 +0900

    Create Node.java
    Node wrapper class for data. Has a pointer to the next node.

---
+++ Java/Stack/Node.java
@@ -0,0 +1,35 @@
+/**
+ * @Author Jay
+ */
+public class Node<T> {
+
+    private T data;
+    private Node<T> nextNode;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                '}';
+    }
+}
