edfca64e66ef18a05152867b1167ee64b0b13795
Jay Lee <thecodingdelight@gmail.com>
2017-04-18 21:51:51 +0900

    Update binarySearchTree.js

--- JavaScript/Data-Structures/Binary-Search-Tree/binarySearchTree.js
+++ JavaScript/Data-Structures/Binary-Search-Tree/binarySearchTree.js
@@ -439,25 +439,3 @@
     };
 
 })(); /* End Binary Search Tree Implementation */
-
-var binarySearchTree = BST();
-// This is pretty much a linked list, since we only have right children.
-binarySearchTree
-    .add(6)
-    .add(3)
-    .add(5)
-    .add(10)
-    .add(7)
-    .add(15)
-    .add(100)
-    .add(1);
-
-console.log(binarySearchTree);
-
-binarySearchTree.postOrderTraversal(function(data, node) {
-    console.log(data);
-});
-
-/*binarySearchTree.remove(2);
-binarySearchTree.remove(4);
-console.log(binarySearchTree);*/
