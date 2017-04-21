3dbc339e2922a92550afc44f784bd528b4cea02d
Jay Lee <thecodingdelight@gmail.com>
2017-04-21 19:07:47 +0900

    Update testBinarySearchTree.js

--- JavaScript/Data-Structures/Binary-Search-Tree/testBinarySearchTree.js
+++ JavaScript/Data-Structures/Binary-Search-Tree/testBinarySearchTree.js
@@ -1,3 +1,9 @@
+
+/**
+ * Example for removing node with a single child.
+ * This is used in the post
+ * */
+
 var binarySearchTree = BST();
 
 binarySearchTree
@@ -7,15 +13,43 @@ binarySearchTree
     .add(10)
     .add(7)
     .add(15)
-    .add(100)
-    .add(1);
+    .add(12)
+    .add(11)
+    .add(13);
+
+binarySearchTree.remove(15);
+
+console.log("Max: " + binarySearchTree.max());
+console.log("Min: " + binarySearchTree.min());
+
+//binarySearchTree.remove(3);
 
 console.log(binarySearchTree);
 
-binarySearchTree.postOrderTraversal(function(data, node) {
-    console.log(data);
+
+binarySearchTree.inOrderTraversal(function(data, node) {
+    console.log("callback data " + data);
 });
 
-/*binarySearchTree.remove(2);
-binarySearchTree.remove(4);
-console.log(binarySearchTree);*/
+
+/**
+ * Example for removing node with two children.
+ * This is also used in the post
+ * */
+var removeTwoChildBst = BST();
+
+removeTwoChildBst
+    .add(6)
+    .add(3)
+    .add(5)
+    .add(1)
+    .add(10)
+    .add(8)
+    .add(7)
+    .add(12)
+    .add(11)
+    .add(13);
+
+removeTwoChildBst.remove(10);
+
+console.log(removeTwoChildBst);
