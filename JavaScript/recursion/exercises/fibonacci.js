cbec461ee921ae1eacfb9adabc37549866aa1d53
Jay Lee <thecodingdelight@gmail.com>
2017-05-27 20:39:06 +0900

    Update fibonacci.js

--- JavaScript/recursion/exercises/fibonacci.js
+++ JavaScript/recursion/exercises/fibonacci.js
@@ -1,5 +1,11 @@
 /**
  * In order to optimize the fibonacci function, use memoization!
+ * fibonacci(5) --> 5
+ * fibonacci(10) --> 55
+ * fibonacci(15) --> 610
+ *
+ * @param {Number} num the nth number of a fibonacci series
+ * @return {Number} the result of the nth number of a fibonacci series.
  */
 function fibonacci(num) {
     if (num <= 2) {
