50d516e1e2fd18136fc91b65ecd1e14768604a7c
Jay Lee <thecodingdelight@gmail.com>
2017-06-16 20:24:21 +0900

    Update fibonacci.js

--- JavaScript/recursion/exercises/fibonacci.js
+++ JavaScript/recursion/exercises/fibonacci.js
@@ -17,3 +17,24 @@ function fibonacci(num) {
     }
     return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
+function fibonacciIter(num) {
+    var result = 1;
+    var counter = 3;
+    if (num <= 2) {
+        return 1;               // First two numbers of fib sequence is equal to 1
+    } else {
+        var prevNo = 1;
+        var prevPrevNo = 1;
+        while (counter < num) {
+            result = prevNo + prevPrevNo;   // xn = xn-1 + xn-2
+            var temp = prevNo;              // Temp variable to store value
+            prevNo = result;                // xn-1 = xn
+            prevPrevNo = temp;              // xn-2 = xn-1. Make sense right?
+            counter++;
+        }
+    }
+    return result;
+}
+
+console.log(fibonacciIter(11));
