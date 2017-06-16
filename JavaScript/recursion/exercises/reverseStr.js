863aff3334ffc4552c79df5fd8a8102c72bb6018
Jay Lee <thecodingdelight@gmail.com>
2017-06-16 20:08:31 +0900

    Update reverseStr.js

--- JavaScript/recursion/exercises/reverseStr.js
+++ JavaScript/recursion/exercises/reverseStr.js
@@ -1,28 +1,34 @@
 /**
-     * @param str the String to reverse.
-     *
-     * Function call returns
-     * reverseStr("troll");
-     * return reverseStr("roll") + "t";
-     * return reverseStr("oll") + "r";
-     * return reverseStr("ll") + "o";
-     * return reverseStr("l") + "l";
-     * return "l";
-     *
-     * Results will bubble up as reverseStr("l") is popped off the call stack,
-     * followed by reverseStr("ll");
-     *
-     * l
-     * ll
-     * llo
-     * llor
-     * llort
-     * */
-    function reverseStr(str) {
-        if (str.length <= 1) {  // to prevent empty string from overflowing call stack
-            return str;
-        }
-        return reverseStr(str.substr(1)) + str.charAt(0);
+ * @param str the String to reverse.
+ *
+ * Function call returns
+ * reverseStr("troll");
+ * return reverseStr("roll") + "t";
+ * return reverseStr("oll") + "r";
+ * return reverseStr("ll") + "o";
+ * return reverseStr("l") + "l";
+ * return "l";
+ *
+ * Results will bubble up as reverseStr("l") is popped off the call stack,
+ * followed by reverseStr("ll");
+ *
+ * l
+ * ll
+ * llo
+ * llor
+ * llort
+ * */
+function reverseStr(str) {
+    if (str.length <= 1) {  // to prevent empty string from overflowing call stack
+        return str;
     }
+    return reverseStr(str.substr(1)) + str.charAt(0);
+}
 
-    console.log(reverseStr("troll"));   // Should return "llort"
+console.log(reverseStr("troll"));   // Should return "llort"
+
+// Iterative approach
+function reverseStrIter(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseStrIter("troll"));  // "llort"
