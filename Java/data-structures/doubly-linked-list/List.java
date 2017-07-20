d25e1fce2aab135b4995006992e5b38f1d6f65c1
Jay Lee <thecodingdelight@gmail.com>
2017-07-20 22:49:32 +0900

    Create List.java

---
+++ Java/data-structures/doubly-linked-list/List.java
@@ -0,0 +1,37 @@
+public interface List<T> {
+
+    /**
+     * Insert data to the front of the list.
+     * */
+    void insert(T data);
+
+    /**
+     * Insert data after data at a specific index.
+     * */
+    void insertAfter(T data, int index);
+
+    /**
+     * Search from the start of the list and find.
+     * data to remove.
+     * */
+    void remove(T data);
+
+    /**
+     * Remove the item at the nth index of the list.
+     *
+     * @param index
+     * @throws IndexOutOfBoundsException
+     * */
+    void removeAt(int index) throws IndexOutOfBoundsException;
+
+    /**
+     * Print items inside of the list.
+     * */
+    void print();
+
+    /**
+     * Get the size of the list.
+     * */
+    int size();
+
+}
