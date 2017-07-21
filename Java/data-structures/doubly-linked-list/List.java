800290badebafd91d2ef9d7125928fdc62e81f7e
Jay Lee <thecodingdelight@gmail.com>
2017-07-21 09:34:57 +0900

    Update List.java

--- Java/data-structures/doubly-linked-list/List.java
+++ Java/data-structures/doubly-linked-list/List.java
@@ -2,17 +2,21 @@ public interface List<T> {
 
     /**
      * Insert data to the end of the list.
+     * @param data the data to add.
      * */
     void insert(T data);
 
     /**
      * Insert data after the node at a specific index.
+     * @param data the data to add.
+     * @param index the index of node after which we will add data.
      * */
     void insertAfter(T data, int index);
 
     /**
      * Search from the start of the list and find
      * data to remove.
+     * @param data the data to remove.
      * */
     void remove(T data);
 
